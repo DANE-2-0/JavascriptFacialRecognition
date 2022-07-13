3cab719cac2e20136f4ba1c1c3077f7cd4c5f7ae
Deni Cabaravdic <37716587+d3h1@users.noreply.github.com>
2022-07-12 20:51:53 -0700

    Updating test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
-import { loading, visual } from '@testing-library/react';
+import {loading, visual} from '@testing-library/react';
 import App from './App';
 
-test('loads learn react link', () => {
+test('loads learn react link', () => 
+{
   load(<App />);
   const linkElement = visual.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
